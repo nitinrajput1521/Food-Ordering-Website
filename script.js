992b241f951ec495b07805fa163c2116e9ce2f77
Nitin Rajput <65024943+nitinrajput1521@users.noreply.github.com>
2024-02-21 13:22:25 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".auto-type",{
+  strings :["DELICIOUSLY '&#129316'","FOOD TREASURE ","HAPPINESS","DELICIOUSNESS"],
+  typeSpeed:200,
+  backSpeed:10,
+  loop:true
+})
+var type = new Typed(".auto-typed",{
+  strings :["Welcome You To The Treasure Of Deliciousness"],
+  typeSpeed:50,
+  backSpeed:50,
+  loop:true
+})
+var types = new Typed(".auto-types",{
+  strings :["DELICIOUSLY WELCOMES YOU"],
+  typeSpeed:500,
+  backSpeed:50,
+  loop:true
+})
